6fa5de099cf137ffad7e7d5a5ad510e152513171
Perokar <framin85@gmail.com>
2021-11-26 12:12:22 +0200

    base posts ok

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ process.env.NTBA_FIX_319 = 1;
 const TelegramApi = require('node-telegram-bot-api');
 const {addNewUser,checkUser, clearUser} = require('./schems/userSchema');
 const token = "2094226777:AAGVqTju7yVGZoZp72UOfSSDO56cG7OPx8k";
+const {addPost, checkPost} = require('./schems/postSchema');
 const bot = new TelegramApi(token, {polling:true});
 
 bot.on("message",   async (msg, prop)=>{
@@ -14,10 +15,14 @@ bot.on("message",   async (msg, prop)=>{
   }
   if (msg.text == "/find") // поиск
   {
-  //  checkUser()
-  bot.sendMessage(msg.from.id, `[Піч на колесах \- DXP](https://youtu.be/8Sh2twEoXBY) Domino's Pizza разом з американською інженерною компанією Roush Enterprises побудували спеціальний автомобіль для доставки піци - Delivery ExPert або DXP. Особливість автомобіля в тому, що він обладнаний вбудованою піччю для постійного підігріву піци, тож клієнт завжди отримує піцу ідеальної температури \- 60 градусів\.`, {parse_mode: 'Markdown', disable_web_page_preview: true})
+          checkPost();
+  // checkUser()
+  //bot.sendMessage(msg.from.id, `[Піч на колесах \- DXP](https://youtu.be/8Sh2twEoXBY) Domino's Pizza разом з американською інженерною компанією Roush Enterprises побудували спеціальний автомобіль для доставки піци - Delivery ExPert або DXP. Особливість автомобіля в тому, що він обладнаний вбудованою піччю для постійного підігріву піци, тож клієнт завжди отримує піцу ідеальної температури \- 60 градусів\.`, {parse_mode: 'Markdown', disable_web_page_preview: true})
   } 
-
+  if (msg.text == "/add") // поиск
+  {
+  addPost();
+  }
   
 if (msg.text == "/start") // Добавление в базу
         {
